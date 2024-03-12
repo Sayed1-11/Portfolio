2230db0b7ab3ae4ece577164fdc648edd44fb885
Sayed1-11 <157264560+Sayed1-11@users.noreply.github.com>
2024-03-12 12:34:55 +0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ['https://sheikh-sayeds-portfolio.onrender.com/',"https://*.127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://sheikh-sayeds-portfolio.onrender.com"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
